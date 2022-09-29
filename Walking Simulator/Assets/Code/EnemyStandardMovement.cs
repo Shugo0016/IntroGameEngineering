140e55c78863243120c4fa55859dcbd16a4df332
Shugo0016 <93568520+Shugo0016@users.noreply.github.com>
2022-09-29 12:32:12 -0700

    Movement features
    Scripts that effect crew member movements

---
+++ Walking Simulator/Assets/Code/EnemyStandardMovement.cs
@@ -0,0 +1,76 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyStandardMovement : MonoBehaviour
+{
+    public float movementSpeed = 20f;
+    public float rotationSpeed = 100f;
+    private bool isWandering = false;
+    private bool isRotatingLeft = false;
+    private bool isRotatingRight = false;
+    private bool isWalking = false;
+
+    Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!isWandering)
+        {
+            StartCoroutine(Wander());
+        }
+        if (isRotatingRight)
+        {
+            transform.Rotate(transform.up * Time.deltaTime * rotationSpeed);
+        }
+        if (isRotatingLeft)
+        {
+            transform.Rotate(transform.up * Time.deltaTime * -rotationSpeed);
+        }
+        if (isWalking)
+        {
+            rb.AddForce(transform.forward * movementSpeed);
+        }
+    }
+
+    IEnumerator Wander()
+    {
+        int rotationTime = Random.Range(1, 3);
+        int rotateWait = Random.Range(1, 3);
+        int rotateDirection = Random.Range(1, 2);
+        int walkWait = Random.Range(1, 3);
+        int walkTime = Random.Range(1, 3);
+
+        isWandering = true;
+
+        yield return new WaitForSeconds(walkWait);
+        isWalking = true;
+
+        yield return new WaitForSeconds(walkTime);
+        isWalking = false;
+
+        yield return new WaitForSeconds(rotateWait);
+        if (rotateDirection == 1)
+        {
+            isRotatingLeft = true;
+            yield return new WaitForSeconds(rotationTime);
+            isRotatingLeft = false;
+        }
+        if (rotateDirection == 2)
+        {
+            isRotatingRight = true;
+            yield return new WaitForSeconds(rotationTime);
+            isRotatingRight = false;
+        }
+        isWandering = false;
+
+    }
+}
