48a561bf0f7e9de0d7282c58f13c40245fe850c2
NiravGolyalla <ng2411@nyu.edu>
2022-09-29 16:05:04 -0400

    Update PublicVars.cs
    for some reason i gotta

--- Walking Simulator/Assets/Code/PublicVars.cs
+++ Walking Simulator/Assets/Code/PublicVars.cs
@@ -1,6 +1,9 @@
 public static class PublicVars {
     public static int score = 0;
+
     public static int Enemies1 = 1;
     public static int Enemies2 = 1;
     public static int Enemies3 = 1;
+    public static float suspicionLevel = 0;
+    public static float frameCounter = 0;
 }
